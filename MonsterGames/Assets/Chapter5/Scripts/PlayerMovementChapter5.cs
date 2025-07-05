7efba5d4b41f3605a336fd72a09b340cb3c24e71
sh4dqwx <bart.karolczak@gmail.com>
2025-07-05 18:55:05 +0200

    Added spawning children

--- MonsterGames/Assets/Chapter5/Scripts/PlayerMovementChapter5.cs
+++ MonsterGames/Assets/Chapter5/Scripts/PlayerMovementChapter5.cs
@@ -16,6 +16,25 @@ public class PlayerMovementChapter5 : MonoBehaviour
     void Update() {
         Vector3 delta = new Vector3(moveInput.x, moveInput.y, 0) * speed * Time.deltaTime;
         Vector3 newPos = transform.position + delta;
+        newPos = ClampPositionToCamera(newPos);
         transform.position = newPos;
+    }
+
+    Vector3 ClampPositionToCamera(Vector3 pos) {
+        Camera cam = Camera.main;
+        if(cam == null)
+            return pos;
+
+        float dist = Mathf.Abs(pos.z - cam.transform.position.z);
+
+        float left = cam.ViewportToWorldPoint(new Vector3(0, 0, dist)).x;
+        float right = cam.ViewportToWorldPoint(new Vector3(1, 0, dist)).x;
+        float bottom = cam.ViewportToWorldPoint(new Vector3(0, 0, dist)).y;
+        float top = cam.ViewportToWorldPoint(new Vector3(0, 1, dist)).y;
+
+        pos.x = Mathf.Clamp(pos.x, left, right);
+        pos.y = Mathf.Clamp(pos.y, bottom, top);
+
+        return pos;
     }
 }
