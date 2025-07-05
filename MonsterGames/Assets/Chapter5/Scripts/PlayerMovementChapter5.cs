37c3fefcaac38e41b262068b25bd1d262e4e99ac
sh4dqwx <bart.karolczak@gmail.com>
2025-07-05 22:48:56 +0200

    added better bounds

--- MonsterGames/Assets/Chapter5/Scripts/PlayerMovementChapter5.cs
+++ MonsterGames/Assets/Chapter5/Scripts/PlayerMovementChapter5.cs
@@ -5,17 +5,27 @@ using UnityEngine.InputSystem;
 public class PlayerMovementChapter5 : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 2f;
+    private float speed = 1f;
     [SerializeField]
     private Animator animator;
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
 
     private Vector2 moveInput = Vector2.zero;
+    private float _width = 0f;
+    private float _height = 0f;
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
         animator.SetFloat("SpeedX", moveInput.x);
         animator.SetFloat("SpeedY", moveInput.y);
+    }
+
+    void Start()
+    {
+        _width = spriteRenderer.bounds.size.x;
+        _height = spriteRenderer.bounds.size.y;
     }
 
     void Update() {
@@ -32,10 +42,10 @@ public class PlayerMovementChapter5 : MonoBehaviour
 
         float dist = Mathf.Abs(pos.z - cam.transform.position.z);
 
-        float left = cam.ViewportToWorldPoint(new Vector3(0, 0, dist)).x;
-        float right = cam.ViewportToWorldPoint(new Vector3(1, 0, dist)).x;
-        float bottom = cam.ViewportToWorldPoint(new Vector3(0, 0, dist)).y;
-        float top = cam.ViewportToWorldPoint(new Vector3(0, 1, dist)).y;
+        float left = cam.ViewportToWorldPoint(new Vector3(0, 0, dist)).x + (_width / 2f);
+        float right = cam.ViewportToWorldPoint(new Vector3(1, 0, dist)).x - (_width / 2f);
+        float bottom = cam.ViewportToWorldPoint(new Vector3(0, 0, dist)).y + (_height / 2f);
+        float top = cam.ViewportToWorldPoint(new Vector3(0, 1, dist)).y - (_height / 2f);
 
         pos.x = Mathf.Clamp(pos.x, left, right);
         pos.y = Mathf.Clamp(pos.y, bottom, top);
