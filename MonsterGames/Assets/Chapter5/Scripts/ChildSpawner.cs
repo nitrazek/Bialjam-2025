8c5b6b3ca088fe21823fde01bb731fc829fb7a8a
sh4dqwx <bart.karolczak@gmail.com>
2025-07-05 18:01:03 +0200

    Added TextMesh Pro, started adding ChildSpawner for generating children

---
+++ MonsterGames/Assets/Chapter5/Scripts/ChildSpawner.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ChildSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private Camera camera;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
